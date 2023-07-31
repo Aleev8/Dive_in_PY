aa5ba38c839f16c94f41594941f4769197cd6a1c
Zirtuff_PC <aleev.alexandr@gmail.com>
2023-07-31 19:02:54 +0600

    solved task_2_2

--- HW/hw_2/task_2_1.py
+++ HW/hw_2/task_2_1.py
@@ -1,18 +1,19 @@
-"""
-Напишите программу, которая получает целое число и возвращает его шестнадцатеричное строковое представление.
-Функцию hex используйте для проверки своего результата.
-"""
+# Напишите программу, которая получает целое число и возвращает его
+# шестнадцатеричное строковое представление.
+# Функцию hex используйте для проверки своего результата.
+
 import random
 
 def my_hex(number: int) -> str:
+    HEX = 16
     result = ''
     chars_hex = list('0123456789abcdef')
     while number > 0:
-        result = chars_hex[number % 16] + result
-        number //= 16
+        result = chars_hex[number % HEX] + result
+        number //= HEX
     return '0x' + result
 
-number = random.randint(1, 1000)
+number = random.randint(1, 100000)
 
 print(f"Случайно сгенерированное число: {number}\n"
       f"Это число в шестнадцитиричной системе счисления: {my_hex(number)}\n"
