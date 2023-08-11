1a588d31b19f0d69aeddc23a9e07904a39048eac
Zirtuff_PC <aleev.alexandr@gmail.com>
2023-08-11 12:15:11 +0600

    solved task_4_2

---
+++ HW/wh_4/task4_2.py
@@ -0,0 +1,15 @@
+# Напишите функцию принимающую на вход только ключевые параметры(kwargs) и возвращающую
+# словарь, где ключ — значение переданного аргумента, а значение — имя аргумента.
+# Если ключ не хешируем, используйте его строковое представление.
+# reverse_kwargs(rev=True, acc="YES", stroka=4) -> {True: "rev", "YES": 'acc', 4: "stroka"}
+
+def kwargs_to_dict(**kwargs):
+    result = {}
+    for key, value in kwargs.items():
+        try:
+            result[value] = key
+        except:
+            result[str(value)] = key
+    return result
+
+print(kwargs_to_dict(rev=True, acc="YES", stroka=4))
